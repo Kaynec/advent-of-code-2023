d2158336634677f4a0c5308800f422faf438d773
kaynec <once462@gmail.com>
2024-02-10 04:30:41 +0330

    changing all  day 10 code

--- DAY-10/part-one.js
+++ DAY-10/part-one.js
@@ -6,7 +6,7 @@ let startingPosCol = inputArray.findIndex(el => el.indexOf('S') >= 0)
 let startingPosRow = inputArray[startingPosCol].indexOf('S')
 
 function findAdjacentElements (col, row) {
-  const hashMap = {}
+  const paths = []
 
   // Top El
   let topEL = inputArray[col - 1][row]
@@ -16,7 +16,7 @@ function findAdjacentElements (col, row) {
     inputArray[col - 1][row] !== '.' &&
     (topEL === '|' || topEL === '7' || topEL === 'F' || topEL === 'J')
   )
-    hashMap[inputArray[col - 1][row]] = [col - 1, row]
+    paths.push([col - 1, row])
 
   // Right Side
   let rightEl = inputArray[col][row + 1]
@@ -25,7 +25,7 @@ function findAdjacentElements (col, row) {
     rightEl !== '.' &&
     (rightEl === '-' || rightEl === '7' || rightEl === 'J')
   )
-    hashMap[inputArray[col][row + 1]] = [col, row + 1]
+    paths.push([col, row + 1])
 
   // left Side
   let leftEl = inputArray[col][row - 1]
@@ -34,7 +34,7 @@ function findAdjacentElements (col, row) {
     leftEl !== '.' &&
     (leftEl === '-' || leftEl === 'L' || leftEl === 'F')
   )
-    hashMap[inputArray[col][row - 1]] = [col, row - 1]
+    paths.push([col, row - 1])
 
   // bottom El
   let bottomEL = inputArray[col + 1][row]
@@ -47,9 +47,9 @@ function findAdjacentElements (col, row) {
       bottomEL === '7' ||
       bottomEL === 'F')
   )
-    hashMap[inputArray[col + 1][row]] = [col + 1, row]
+    paths.push([col + 1, row])
 
-  return hashMap
+  return paths
 }
 
 const startingPaths = findAdjacentElements(startingPosCol, startingPosRow)
@@ -61,74 +61,77 @@ console.log(startingPaths)
 // |F--J
 // LJ...
 
-for (let [key, [col, row]] of Object.entries({ '|': [3, 0] })) {
-  const steps = []
-  let step = 0
+const allVisited = []
+const firstVisited = []
+const secondVisited = []
+
+let [firstPath, secondPath] = startingPaths[0]
+let [fircol, firrow] = firstPath
+let [seccol, secrow] = secondPath
+
+while (true) {
+  ;[fircol, curcol] = pathMove(fircol, firrow)
+  ;[seccol, secrow] = pathMove(seccol, secrow)
+
+  firstVisited.push(fircol, curcol)
+  secondVisited.push(seccol, secrow)
+  if (
+    firstVisited.some(([col, row]) =>
+      secondVisited.find(([col2, row2]) => col === col2 && row === row2)
+    )
+  )
+    break
+}
+
+function pathMove (col, row) {
   function updateCordinates (col, row) {
     if (!visited.find(([elc, elr]) => elc === currentCol && elr === currentRow))
       visited.push([currentCol, currentRow])
     currentCol = col || currentCol
     currentRow = row || currentRow
     currentEl = inputArray[currentCol][currentRow]
-    step++
   }
 
   function findIfVisited (col, row) {
-    const el = inputArray[col][row]
-
-    console.log(
-      inputArray[currentCol][currentRow],
-      el,
-      !!visited.find(([elc, elr]) => {
-        return elc === col && elr === row
-      }),
-      currentCol,
-      currentRow
-    )
-    // if (el === 'S') {
-    //   return visited.length <= 1
-    // }
-    return !!visited.find(([elc, elr]) => {
+    return visited.some(([elc, elr]) => {
       return elc === col && elr === row
     })
   }
 
   let currentCol = col
   let currentRow = row
-  currentEl = key
+  let currentEl = inputArray[currentCol][currentRow]
   const visited = [[startingPosCol, startingPosRow]]
 
-  while (currentEl !== 'S') {
-    // console.log(visited)
-    if (currentEl === '|') {
-      if (findIfVisited(currentCol + 1, currentRow))
-        updateCordinates(currentCol - 1)
-      else updateCordinates(currentCol + 1)
-    }
-    // if the one above is in visited then go to right and if right is visited to left
-    else if (currentEl === 'L') {
-      if (findIfVisited(currentCol - 1, currentRow))
-        updateCordinates(currentCol, currentRow + 1)
-      else updateCordinates(currentCol - 1)
-    }
-    // if the one Left is in visited then go to right and if right is visited to left
-    else if (currentEl === 'J') {
-      if (findIfVisited(currentCol, currentRow - 1))
-        updateCordinates(currentCol - 1)
-      else updateCordinates(currentCol, currentRow - 1)
-    } else if (currentEl === 'F') {
-      if (findIfVisited(currentCol, currentRow + 1))
-        updateCordinates(currentCol + 1)
-      else updateCordinates(currentCol, currentRow + 1)
-    } else if (currentEl === '-') {
-      if (findIfVisited(currentCol, currentRow - 1))
-        updateCordinates(currentCol, currentRow + 1)
-      else updateCordinates(currentCol, currentRow - 1)
-    } else if (currentEl === '7') {
-      if (findIfVisited(currentCol + 1, currentRow))
-        updateCordinates(currentCol, currentRow - 1)
-      else updateCordinates(currentCol + 1)
-    }
+  // console.log(visited)
+  if (currentEl === '|') {
+    if (findIfVisited(currentCol + 1, currentRow))
+      updateCordinates(currentCol - 1)
+    else updateCordinates(currentCol + 1)
   }
-  console.log(visited)
+  // if the one above is in visited then go to right and if right is visited to left
+  else if (currentEl === 'L') {
+    if (findIfVisited(currentCol - 1, currentRow))
+      updateCordinates(currentCol, currentRow + 1)
+    else updateCordinates(currentCol - 1)
+  }
+  // if the one Left is in visited then go to right and if right is visited to left
+  else if (currentEl === 'J') {
+    if (findIfVisited(currentCol, currentRow - 1))
+      updateCordinates(currentCol - 1)
+    else updateCordinates(currentCol, currentRow - 1)
+  } else if (currentEl === 'F') {
+    if (findIfVisited(currentCol, currentRow + 1))
+      updateCordinates(currentCol + 1)
+    else updateCordinates(currentCol, currentRow + 1)
+  } else if (currentEl === '-') {
+    if (findIfVisited(currentCol, currentRow - 1))
+      updateCordinates(currentCol, currentRow + 1)
+    else updateCordinates(currentCol, currentRow - 1)
+  } else if (currentEl === '7') {
+    if (findIfVisited(currentCol + 1, currentRow))
+      updateCordinates(currentCol, currentRow - 1)
+    else updateCordinates(currentCol + 1)
+  }
+  return [currentCol, currentRow]
 }
