098425b5fa599b015c78ecaeaa0cff5c7fb17fbc
kaynec <once462@gmail.com>
2024-02-12 03:02:30 +0330

    day 10 is too hard

--- DAY-10/part-one.js
+++ DAY-10/part-one.js
@@ -1,6 +1,7 @@
 const { readFileSync } = require('fs')
+const { connect } = require('http2')
 
-let inputArray = readFileSync('input', 'utf-8').split('\r\n')
+let inputArray = readFileSync('sample', 'utf-8').split('\r\n')
 
 let startingPosCol = inputArray.findIndex(el => el.indexOf('S') >= 0)
 let startingPosRow = inputArray[startingPosCol].indexOf('S')
@@ -10,6 +11,10 @@ const directions = {
     chars: ['7', 'F', 'J', '|'],
     dir: [-1, 0]
   },
+  BOTTOM: {
+    chars: ['7', 'F', 'L', '|'],
+    dir: [+1, 0]
+  },
   RIGHT: {
     chars: ['-', '7', 'J'],
     dir: [0, +1]
@@ -17,10 +22,6 @@ const directions = {
   LEFT: {
     chars: ['-', 'L', 'F'],
     dir: [0, -1]
-  },
-  TOP: {
-    chars: ['7', 'F', 'L', '|'],
-    dir: [+1, 0]
   }
 }
 
@@ -31,7 +32,7 @@ function findAdjacentElements (col, row) {
     const { chars, dir } = directions[key]
     const [dirCol, dirRow] = dir
     const [newCol, newRow] = [col + dirCol, row + dirRow]
-    const newChar = inputArray[newCol][newRow]
+    const newChar = !!inputArray[newCol] ? inputArray[newCol][newRow] : 'NULL'
     const isAdjacent = chars.includes(newChar)
     if (isAdjacent) paths.push([newCol, newRow])
   }
@@ -44,7 +45,7 @@ const startingPaths = findAdjacentElements(startingPosCol, startingPosRow)
 let [firstPath, secondPath] = startingPaths
 let [fircol, firrow] = firstPath
 let [seccol, secrow] = secondPath
-const visited = []
+const visited = [[startingPosCol, startingPosRow]]
 
 let step = 0
 
@@ -52,11 +53,10 @@ function pathMove (col, row) {
   let curCol = col
   let curRow = row
   let currentEl = inputArray[curCol][curRow]
-  const findIfVisited = (col = curCol, row = curRow) =>
-    visited.some(([elc, elr]) => elc === col && elr === row) ??
-    inputArray[col][row] === 'S'
+  const findIfVisit = (col = curCol, row = curRow) =>
+    visited.some(([elc, elr]) => elc === col && elr === row)
 
-  function updateCordinates (col, row) {
+  function updatePos (col, row) {
     if (!visited.find(([elc, elr]) => elc === curCol && elr === curRow))
       visited.push([curCol, curRow])
     curCol = col ?? curCol
@@ -65,32 +65,30 @@ function pathMove (col, row) {
   }
 
   if (currentEl === '|') {
-    if (findIfVisited(curCol + 1, curRow)) return updateCordinates(curCol - 1)
-    return updateCordinates(curCol + 1)
+    if (findIfVisit(curCol + 1, curRow)) return updatePos(curCol - 1)
+    return updatePos(curCol + 1)
   }
   if (currentEl === 'L') {
-    if (findIfVisited(curCol - 1, curRow))
-      return updateCordinates(curCol, curRow + 1)
-    return updateCordinates(curCol - 1)
+    if (findIfVisit(curCol - 1, curRow)) return updatePos(curCol, curRow + 1)
+    return updatePos(curCol - 1)
   }
   if (currentEl === 'J') {
-    if (findIfVisited(curCol, curRow - 1)) return updateCordinates(curCol - 1)
-    return updateCordinates(curCol, curRow - 1)
+    if (findIfVisit(curCol, curRow - 1)) return updatePos(curCol - 1)
+    return updatePos(curCol, curRow - 1)
   }
   if (currentEl === 'F') {
-    if (findIfVisited(curCol, curRow + 1)) return updateCordinates(curCol + 1)
-    return updateCordinates(curCol, curRow + 1)
+    if (findIfVisit(curCol, curRow + 1)) return updatePos(curCol + 1)
+    return updatePos(curCol, curRow + 1)
   }
   if (currentEl === '-') {
-    if (findIfVisited(curCol, curRow - 1))
-      return updateCordinates(curCol, curRow + 1)
-    return updateCordinates(curCol, curRow - 1)
+    if (findIfVisit(curCol, curRow - 1)) return updatePos(curCol, curRow + 1)
+    return updatePos(curCol, curRow - 1)
   }
   if (currentEl === '7') {
-    if (findIfVisited(curCol + 1, curRow))
-      return updateCordinates(curCol, curRow - 1)
-    return updateCordinates(curCol + 1)
+    if (findIfVisit(curCol + 1, curRow)) return updatePos(curCol, curRow - 1)
+    return updatePos(curCol + 1)
   }
+  return [curCol, curRow]
 }
 
 let element = null
@@ -102,4 +100,123 @@ while (!element) {
   if (fircol === seccol && firrow === secrow) element = [fircol, firrow]
   step++
 }
-console.log(element, (step += 1))
+
+let diffs = []
+
+visited.sort()
+let loopColLength = 0
+let loopRowLength = 0
+
+for (let i = 0; i < visited.length - 1; i++) {
+  const currentEl = visited[i]
+  const nextEl = visited[i + 1]
+  const rowDiff = nextEl[1] - currentEl[1] - 1
+  loopColLength = Math.max(loopColLength, nextEl[0])
+  loopRowLength = Math.max(loopRowLength, nextEl[1])
+  if (rowDiff >= 1) {
+    diffs.push({
+      element: [currentEl[0], currentEl[1] + 1],
+      diff: rowDiff,
+      connected: []
+    })
+  }
+}
+
+// function recurse (col, row) {
+//   let tile = 0
+
+//   function walk (col, row) {
+//     // console.log(col, row, 'walking')
+//     if (!inputArray[col] || !inputArray[col][row]) {
+//       // console.log('not existo', col, row)
+//       return
+//     }
+//     if (inputArray[col] && inputArray[col][row]) {
+//       console.log('existo', inputArray[col][row])
+//       if (visited.find((elc, elr) => elc === col && elr === row)) {
+//         // console.log('existo in visito')
+
+//         return
+//       }
+//     }
+
+//     walk(col - 1, row)
+//     walk(col + 1, row)
+//     walk(col, row + 1)
+//     walk(col, row - 1)
+//   }
+//   walk(col, row)
+//   console.log({ tile })
+// }
+
+// recurse(3, 2)
+
+// for (let { element, diff } of [{ element: [3, 2], diff: 5 }]) {
+//   for (let i = 1; i <= diff; i++) {
+//     curr = inputArray[element[0]][element[1] + i]
+//   }
+// }
+let count = 0
+
+function recorsivePrinter (connected) {
+  function recursive () {
+    while (connected.length) {
+      const item = connected.pop()
+      let itemFromDiff = diffs.findIndex(
+        el => el.element[0] === item[0] && el.element[1] === item[1]
+      )
+      if (itemFromDiff >= 0) {
+        const [col, row] = diffs[itemFromDiff].element
+        inputArray[col][row] = '0'
+        diffs.splice(itemFromDiff, 1)
+      }
+
+      recorsivePrinter(diffs[itemFromDiff].connected)
+    }
+  }
+
+  recursive()
+}
+
+function findAdjacentElementsOfdiffs (col, row, connected) {
+  let isFalse = false
+
+  objectLoop: for (let key of Object.keys(directions)) {
+    const { dir } = directions[key]
+    const [dirCol, dirRow] = dir
+    const [newCol, newRow] = [col + dirCol, row + dirRow]
+
+    if (!inputArray[newCol] || !inputArray[newCol][newRow]) {
+      isFalse = true
+      continue objectLoop
+    }
+    if (visited.some(([elc, elr]) => newCol === elc && newRow === elr)) {
+      continue objectLoop
+    }
+
+    connected.push([newCol, newRow])
+  }
+
+  if (isFalse) {
+    recorsivePrinter(connected)
+  }
+}
+
+for (let { element, diff } of diffs) {
+  const [col, row] = element
+  for (let i = 1; i < diff; i++) {
+    diffs.push({
+      element: [col, row + i],
+      diff: 0,
+      connected: []
+    })
+  }
+}
+
+diffs.sort((a, b) => a.element[0] - b.element[0])
+
+for (let { element, connected } of diffs) {
+  const [col, row] = element
+  findAdjacentElementsOfdiffs(col, row, connected)
+}
+console.log(diffs)
